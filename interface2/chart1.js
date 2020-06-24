9f226ac6e2d802e52390610338912cb8cecbe388
chrisbryan <cbryan16@asu.edu>
2020-06-23 17:17:32 -0700

    Ajdusted layout for interface 3

--- interface2/chart1.js
+++ interface2/chart1.js
@@ -10,15 +10,14 @@ function make_chart1(){
 
   //Defining the margin conventions
   var margin = {top: 50, right: 30, bottom: 50, left: 70},
-      width = 800 - margin.left - margin.right,
+      width = 1200 - margin.left - margin.right,
       height = 400 - margin.top - margin.bottom;
 
   //define svg
   var svg = d3.select("#bloc2")
               .append("svg")
-              .attr("width", 2 * width + margin.left + margin.right)
+              .attr("width", width + margin.left + margin.right)
               .attr("height", height + margin.top + 2* margin.bottom)
-
               .append("g")
               .attr("transform", "translate(0, " + margin.top+")")
    
@@ -34,7 +33,7 @@ function make_chart1(){
 
   // set the ranges
   var x = d3.scaleBand()
-            .range([0, 2 * width])
+            .range([0, width])
             // .bandwidth()
             .padding(0.1);
 
@@ -69,9 +68,8 @@ function make_chart1(){
 
     // Scale the range of the data in the domains
     x.domain(data_for_chart1.map(function(d) { return d.Label; }));
-    var yMax = d3.max(data_for_chart1,function(d){return d.NumPerson;})
-    // var yMin = d3.min(data_for_chart1,function(d){return d.NumPerson;})
-    y.domain([0, yMax]);
+    // var yMax = d3.max(data_for_chart1,function(d){return d.NumPerson;})
+    y.domain([0, 14]);
 
 
     svg.selectAll("bar")
@@ -80,7 +78,7 @@ function make_chart1(){
       .style("fill", "steelblue")
       .attr("x", function(d) { return x(d.Label); })
       .attr("width", x.bandwidth())
-      .attr("y", function(d) { return y(d.NumPerson); })
+      .attr("y", function(d,i) { return y(d.NumPerson); })
       .attr("height", function(d) { return height - y(d.NumPerson); });
 
     // add the x Axis
@@ -88,25 +86,52 @@ function make_chart1(){
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x).ticks(10))
       .selectAll("text")  
-        .attr("y", 0)
-        .attr("x", 9)
+        .attr("y", 10)
+        .attr("x", 10)
+        .style('font-size','14')
         .attr("dy", ".35em")
-        .attr("transform", "rotate(90)")
+        .attr("transform", "rotate(45)")
         .style("text-anchor", "start");
 
-
-
-    //Draw y axis
-    d3.select("#bloc1")
-      .append("svg")
-      .attr("height",height + margin.top + margin.bottom)
+    //Draw y axis and label
+    bloc1Svg = d3.select("#bloc1").append("svg");
+    bloc1Svg.attr("height",height + margin.top + margin.bottom)
       .attr("width",margin.left)
       .append("g")
       .attr("transform", "translate(70,"+ margin.top+")")
       .attr("class", "y-axis ")
-      .call(d3.axisLeft(y))
-      .selectAll(".tick line")
-      // .attr("stroke-dasharray", "1, 2");  
-             
+      .call(d3.axisLeft(y));
+    bloc1Svg.append("line")
+      .attr("x1",70)
+      .attr("y1",margin.top)
+      .attr("x2",70)
+      .attr("y2",height+margin.bottom)
+      .attr("stroke","black")
+      .attr("stroke-width","2")
+    bloc1Svg.append("g")
+      .attr("transform","translate(30,"+height*.65+")")
+      .append("text") 
+      .attr("x",0)
+      .attr("y",0)
+      .attr('text-anchor','middle')
+      .style("font-size","18px")
+      .attr("transform", "rotate(-90)")
+      .text("Number of people with this item")
+
+    // Draw the top and right-side outlines of the bar chart
+    svg.append("line")
+      .attr("x1",0)
+      .attr("y1",0)
+      .attr("x2",width)
+      .attr("y2",0)
+      .attr("stroke","black")
+      .attr("stroke-width","1");
+    svg.append("line")
+      .attr("x1",width)
+      .attr("y1",0)
+      .attr("x2",width)
+      .attr("y2",height)
+      .attr("stroke","black")
+      .attr("stroke-width","1");
   });
 }
--- interface2/chart2.js
+++ interface2/chart2.js
@@ -11,7 +11,7 @@ function make_chart2(){
 
   //Defining the margin conventions
   var margin = {top: 50, right: 30, bottom: 50, left: 70},
-      width = 400 - margin.left - margin.right,
+      width = 500 - margin.left - margin.right,
       height = 400 - margin.top - margin.bottom;
 
   var x = d3.scaleLinear()
@@ -94,6 +94,8 @@ function make_chart2(){
 
     var yAxis = d3.axisLeft(y);
     
+
+
     svg.selectAll(".dot")
       .data(data_for_chart2)
     .enter().append("circle")
@@ -124,6 +126,7 @@ function make_chart2(){
       .attr("class", "label")
       .attr("x", width)
       .attr("y", -6)
+      .attr("class", "y-axis ")
       .style("text-anchor", "end")
       .text("Sepal Width (cm)");
 
@@ -137,6 +140,24 @@ function make_chart2(){
         .attr("dy", ".71em")
         .style("text-anchor", "end")
         .text("Sepal Length (cm)")
-             
+       
+    // Label the x- and y-axes
+    svg.append("g")
+      .attr("transform","translate(-40,"+(height/2)+")")
+      .append("text") 
+      .attr('text-anchor','middle')
+      .attr("x",0)
+      .attr("y",0)
+      .style("font-size","18px")
+      .attr("transform", "rotate(-90)")
+      .text("Y-axis label here")
+    svg.append("g")
+      .attr("transform","translate("+width/2+","+(height+45)+")")
+      .append("text") 
+      .attr('text-anchor','middle')
+      .attr("x",0)
+      .attr("y",0)
+      .style("font-size","18px")
+      .text("X-axis label here")
   });
 }
